02616562066ebcec729b8e1e48d3520f128f29e9
Jonathas Fiscina <thefiscina@gmail.com>
2022-07-16 20:01:42 -0300

    acertos

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 import type * as React from "react";
 import { StyleProp, ViewStyle, TextStyle } from "react-native";
 
-declare module "react-native-select-dropdown" {
-  export type SelectDropdownProps = {
+declare module "select-search" {
+  export type SelectSearchProps = {
     /**
       * array of data that will be represented in dropdown, can be array of objects
       */
@@ -133,7 +133,7 @@ declare module "react-native-select-dropdown" {
     renderSearchInputRightIcon?: (selectedItem: any, index: number) => React.ReactNode;
   };
 
-  export default class SelectDropdown extends React.Component<SelectDropdownProps> {
+  export default class SelectSearch extends React.Component<SelectSearchProps> {
     /**
      * Remove selection & reset it to display defaultButtonText check
      */
